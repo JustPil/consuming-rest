5cc465e43e3f4b712ef9ccdddbac11b928bfaba8
justpil <88640664+JustPil@users.noreply.github.com>
2022-11-07 21:49:31 -0800

    Added unit test

--- src/test/java/justpil/consumingrest/ConsumingRestApplicationTests.java
+++ src/test/java/justpil/consumingrest/ConsumingRestApplicationTests.java
@@ -1,13 +1,17 @@
 package justpil.consumingrest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class ConsumingRestApplicationTests {
+	@Autowired
+	private RestTemplate template;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assert template != null;
 	}
-
 }
